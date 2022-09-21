9d06ffe467479af087860e58e8dd72285dc81870
Rahul Sandireddy <76141102+Rahulrayudu@users.noreply.github.com>
2022-09-21 10:17:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,4 +67,6 @@ ggsave("helloworld.png", width = 8, height = 8)
   st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1 = process1.communicate()
-st.write(result1)
+
+image = Image.open('helloworld.png')
+st.image(image)
