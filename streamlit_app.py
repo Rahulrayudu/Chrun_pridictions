38902bbbc86f4a038fd5a37eaefba80703a20adc
Rahul Sandireddy <76141102+Rahulrayudu@users.noreply.github.com>
2022-09-21 10:08:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ st.header('🎈 R x Python Streamlit App')
 
 st.sidebar.markdown('''
 # About us
-Rahul Sandireddy - 20BCE1001
-Vikkurty Vamsi   - 20BCE1066
-Thota Varshika   - 20BPS1158
-Jaladi Deepika   - 20BPS1099
+-Rahul Sandireddy - 20BCE1001
+-Vikkurty Vamsi   - 20BCE1066
+-Thota Varshika   - 20BPS1158
+-Jaladi Deepika   - 20BPS1099
 
 R packages used:
 - 'ISLR'
@@ -67,6 +67,5 @@ ggsave("helloworld.png", width = 8, height = 8)
   st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1 = process1.communicate()
-st.write(result1)
 image = Image.open('helloworld.png')
 st.image(image)
